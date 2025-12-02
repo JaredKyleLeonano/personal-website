5eb5043b46cf8834dfbbc2d94317cdf9dde13286
JaredKyleLeonano <jaredleonano.3@gmail.com>
2025-12-01 22:29:38 -0400

    implemented dvh pt.2

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -21,7 +21,7 @@ const Works = ({
   return (
     <section
       ref={sectionRef}
-      className="flex justify-center items-center h-[100dvh] w-screen relative [background:radial-gradient(ellipse_250%_100%_at_50%_50%,#0B2F2F_0%,black_60%)] snap-center"
+      className="flex justify-center items-center h-dvh w-screen relative [background:radial-gradient(ellipse_250%_100%_at_50%_50%,#0B2F2F_0%,black_60%)] snap-center"
     >
       <div className="absolute inset-0 pointer-events-none opacity-20 bg-[repeating-linear-gradient(0deg,transparent,transparent_2px,rgba(45,212,191,0.03)_2px,rgba(45,212,191,0.03)_4px)]" />
       <div className="mt-16 relative overflow-y-scroll border-3 border-teal-500 rounded-xl bg-black/30 shadow-[0_0_25px_#22D3EE] snap-y snap-mandatory h-[75%] w-[90%] md:h-[60%] md:w-[80%]">
